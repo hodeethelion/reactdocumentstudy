fb64d75b6e9fee2f987a47a044a50186376a11b1
hodee <tlsghk8619@gmail.com>
2023-08-14 19:33:33 +0900

    Add: tailwind css added

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Dropdownsample from "./Dropdownsample.jsx";
 function App() {
   return (
     <div className="App">
-      <h1>React study</h1>
-      <h2>Dropdown</h2>
+      <h1 className="text-6xl">React study</h1>
+      <div className="text-3xl font-light">Dropdown</div>
       <Dropdownsample />
     </div>
   );
