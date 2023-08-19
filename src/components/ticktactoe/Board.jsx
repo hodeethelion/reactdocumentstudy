cc82cc2f238458d8ec87a61644313745b629e6ad
hodee <tlsghk8619@gmail.com>
2023-08-19 14:58:41 +0900

    feat: Game finished

--- src/components/ticktactoe/Board.jsx
+++ src/components/ticktactoe/Board.jsx
@@ -4,10 +4,43 @@ import Square from "./Square";
 const Board = () => {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  
 
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
   const handleClick = (i) => {
     // 복사하기
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -24,6 +57,7 @@ const Board = () => {
   return (
     <div>
       <div>
+        <div className="status text-center font-thin">{status}</div>
         <div className="board-row flex">
           <Square
             value={squares[0]}
